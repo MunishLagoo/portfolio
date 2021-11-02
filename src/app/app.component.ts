8f3fa5b6aaead65cdbfaf192593715ea85ffe037
Munish Lagoo <munishlagoo@gmail.com>
2021-11-02 19:12:01 -0400

    add services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,49 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AccountService } from './services/account.service';
+import { Stock } from './services/stocks.model';
+import { StocksService } from './services/stocks.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [StocksService],
 })
 export class AppComponent implements OnInit, OnDestroy {
+  refresh: boolean = true;
+  stocks: Stock[] = [];
+  interval: any;
 
-  constructor() { }
+  constructor(private accountService: AccountService,
+              private stocksService: StocksService) {}
 
   ngOnInit(): void {
+    this.load();
 
+    this.interval = setInterval(() => {
+      if (this.refresh) {
+        this.load();
+      }
+    }, 15000)
   }
 
   ngOnDestroy(): void {
-
+    clearInterval(this.interval);
   }
 
   toggleRefresh(): void {
-
+    this.refresh = !this.refresh;
   }
 
   reset(): void {
-
+    this.accountService.reset();
   }
 
   private load(): void {
-
+      this.stocksService.getStocks().subscribe(stocks => {
+        this.stocks = stocks;
+      }, error => {
+        console.error(`THere was an error loading stocks: ${error}`);
+      });
   }
 }
